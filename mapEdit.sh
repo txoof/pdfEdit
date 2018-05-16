135d8073e9c62baaafb4dd0dafe40b7c9bc3855e
txoof <aaron.ciuffo@gmail.com>
2018-05-16 10:27:45 +0200

    add decompression

--- mapEdit.sh
+++ mapEdit.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-myname='com.txoof.'`basename $0`
+# written by Aaron Ciuffo (aaron.ciuffo@gmail.com)
+# 2018-05-18
+
+# bulk edit MAP test results; find and replace "Student ID: " with
+# "StudentID:" to improve compatability with Alert Solutions in PowerSchool
+# for mailing test results to parents
+
+myLongName='com.txoof.'`basename $0`
+myName=$0
+
 if [ $# -lt 1 ]
 then
+  echo $myName: edit individual MAP reports into file ready for AlertSolutions
+  echo usage: $myName doc1.pdf doc2.pdf doc3.pdf
   echo supply at least one file name to process. exiting.
   exit 0
 fi
 
-tmpdir='/tmp/'$myname
+tmpdir='/tmp/'$myLongName
 
 if [ ! -d $tmpdir ]
 then
@@ -18,9 +29,49 @@ then
   fi
 fi
 
+echo found $# files 
+failure=()
+decompressed=()
+edited=()
+compressed=()
+added=()
 for each in "$@"
 do
   base=`basename "$each"`
-  # check for failures and add to array of failures to report on later
-  ./cpdf -decompress "$each" -o $tmpdir'/de'"$base"
+
+  # decompress the PDF with CPDF
+  decompFile="$tmpdir"'/de'"$base"
+  if ./cpdf -decompress "$each" -o "$decompFile" > /dev/null 2>&1
+  then
+    decompressed+=("$decompFile")
+  else
+    failure+=("$each")
+    echo FAILURE: "$each"
+    continue
+  fi
 done
+
+for each in $decompressed
+do
+  # find and replace with sed
+  if sed -e "s/Student ID: /StudentID:/g"
+  then
+    failure+=("$each")
+  else
+    edited+=("$each")
+  fi
+
+  # compress the PDF
+
+
+done
+
+# combine the individual PDFs into a massive PDF
+
+for fail in "$failure"
+do
+  echo Failed to process: "$fail"
+done
+
+echo ----DECOMPRESSED: ${#decompressed[@]}----
+echo ----EDITED: ${#edited[@]}----
